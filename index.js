c591e6291a0d8e5dd22661cf4bfb562f9e18e95e
arta fallahpoor <102357381+artafps@users.noreply.github.com>
2025-03-29 23:56:59 +0330

    next update check my code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const data = 'awdawdwad'
 console.log(data);
 function addNumbers(a, b) {
-    return a + b;
+    return a * b; // اشتباه: اینجا باید جمع (+) باشه نه ضرب (*)
 }
 
 console.log(addNumbers(3, 5)); // انتظار داریم 8 باشه، ولی 15 چاپ میشه!
